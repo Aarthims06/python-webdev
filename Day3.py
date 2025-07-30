41fd31343340aa8fa33286995f0444dc37d7037e
Aarthims06 <2006sixfire2006@gmail.com>
2025-07-30 20:10:24 +0530

    Day3

---
+++ Day3.py
@@ -0,0 +1,49 @@
+#if-else
+if(True):
+    print("yes")
+else:
+    print("no")
+
+#comparision operator
+print("win"=="win")     #returns true if both side is same
+print("win"=="winn")
+
+name = "zara"
+if(name == "zara"):
+    print("Yes Iam Zara ")
+else:
+    print("No Iam not")
+
+name = "Aasha"
+if(name == "zara"):
+    print("Yes Iam Zara ")
+else:
+    print("No Iam not")
+
+#task1
+Meghna = input("Is Meghna Died or alive ?")
+if(Meghna == "Died"):
+    print("Surya meets priya")
+else:
+    print("Surya weds Meghna")
+
+#task2
+mark = int(input("enter your mark:"))
+if(mark>35):
+    print("pass")
+else:
+    print("fail")
+
+#task3
+income  = int(input("enter your income:"))
+if(income>7000):
+    print("Scholarship is available")
+else:
+    print("Not eligible for scholarship")
+
+#task4
+a = int(input("Enter a number:"))
+if(a%3==0 and a%5==0):
+    print("Yes")
+else:
+    print("no")
